489021a2bf7c12e324c72529c8639b802b71eebf
Guilherme Oliveira <guilherme.061@gmail.com>
2016-03-14 14:25:45 -0300

    Update cco-interpolacaonewton.r

--- cco-interpolacaonewton.r
+++ cco-interpolacaonewton.r
@@ -1,12 +1,24 @@
-interpolacao_newton <- function(x, y, zi, n)
+# Interpolação pelo Método de Newton
+# 
+# O algoritmo deve:
+# 	Poder receber vários pontos para serem interpolados (ok)
+# 	Imprimir a forma geral do polinômio (ok)
+# 	Plotar o gráfico (ok)
+# 	Calcular o erro (relativo, verdadeiro, percentual) (ok)
+# 	Ao invés de passar os pontos, passar a função
+
+
+interpolacao_newton <- function(x, y, zi, n, valor_verdadeiro = NULL)
 {
 	# x -> conjunto de valores no eixo x
 	# y -> conjunto de valores no eixo y
 	# zi -> valores a serem interpolados
 	# n -> quantidade de pontos
+	# valor_verdadeiro <- valor verdadeiro de um ponto que deseja se calcular o erro
 	
 	y_resultado <- c()
 	B <- y
+	erro <- 0
 	
 	for(k in 1:(n-1))
 	{
@@ -16,12 +28,23 @@ interpolacao_newton <- function(x, y, zi, n)
 		}
 	}
 	
+	
+	# Avaliando o valor do polinômio nos pontos Z
+	
 	for(z in zi)
 	{
 		R <- B[n]
 		for (i in (n-1):1)
 		{
 			R <- R * (z - x[i]) + B[i]
+			
+		}
+		
+		if(!missing(valor_verdadeiro) && length(zi) == 1) 
+		{
+			erro <- abs((valor_verdadeiro - R) / valor_verdadeiro)
+			string_erro <- sprintf("Erro Relativo Verdadeiro: %f%%", erro*100)
+			print(string_erro)
 		}
 	y_resultado <- c(y_resultado, R)
 	}
@@ -43,6 +66,46 @@ interpolacao_newton <- function(x, y, zi, n)
 	print(resultado)
 	print(polinomio)
 	return (y_resultado)
+}
+
+funcao <- function(Xs)
+{
+	return (1 / (1 + (25*Xs*Xs)))
+}
+
+exercicio_geral <- function(grau, intervalo_menor, intervalo_maior, nro_pontos = 100 )
+{
+	if(missing(nro_pontos))
+	{
+		nro_pontos <- grau
+	}
+	
+	x_func <- seq(from=intervalo_menor, to=intervalo_maior, length.out=(grau+1))
+	y_func <- funcao(x_func)
+	x_plot <- seq(from=intervalo_menor, to=intervalo_maior, length.out = nro_pontos)
+	
+	y_interpolacao <- interpolacao_newton(x_func, y_func, x_plot, length(x_func))
+	y_verdadeiro <- funcao(x_plot)
+	erro_verdadeiro <- abs((y_verdadeiro - y_interpolacao)/ y_verdadeiro)
+	string_erro <- sprintf("Media dos Erros: %f%%", mean(erro_verdadeiro)*100)
+	print(string_erro)
+	
+	plot(x_plot, y_interpolacao, type="l", col="green")
+	lines(x_plot, y_verdadeiro, col="red")
+	eixo_x <- seq(from=intervalo_menor, to=intervalo_maior, length.out=5)
+	lines(eixo_x,0 * eixo_x ,col="black")
+}
+
+exercicio <- function()
+{
+	x_func <- seq(from=-1, to=1, length.out=11)
+	y_func <- (1 / (1 + (25*x_func*x_func)))
+	x_plot <- seq(from=-1, to=1, length.out=100)
+	
+	y_interpolacao <- interpolacao_newton(x_func, y_func, x_plot,length(x_func))
+	
+	plot(x_plot, y_interpolacao, type="l", col="green")
+	lines(x_plot, (1 / (1 + (25*x_plot*x_plot))), col="red")
 }
 
 plotar <- function()
